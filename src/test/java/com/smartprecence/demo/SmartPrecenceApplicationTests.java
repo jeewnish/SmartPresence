8e153e6199de9b6c77183e45e463861c10634c0f
jeewnish <170529823+jeewnish@users.noreply.github.com>
2026-02-22 17:57:50 +0530

    entity and 2 repositories

---
+++ src/test/java/com/smartprecence/demo/SmartPrecenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprecence.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPrecenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
