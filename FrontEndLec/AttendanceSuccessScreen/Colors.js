20b6b980f3fca7fd3113d8a95c8ba66acd9ca720
Pasindu Denuwan <denuwan672@gmail.com>
2026-06-17 23:37:07 +0530

    add colors js file to front end of lec side

---
+++ FrontEndLec/AttendanceSuccessScreen/Colors.js
@@ -0,0 +1,34 @@
+// constants/Colors.js
+const Colors = {
+  // Backgrounds
+  bgDeep:        '#0B0C1E',
+  bgDark:        '#0E0F24',
+  bgCard:        '#C8CAE0',
+  bgCardInner:   '#D0D2E8',
+
+  // Purple / blue palette
+  purplePrimary: '#5B35CC',
+  purpleLight:   '#7B5FFF',
+  purpleGlow:    'rgba(91, 53, 204, 0.45)',
+  purpleDeep:    '#3318A8',
+  neonBlue:      '#4A6CF7',
+  neonPurple:    '#8B6BFF',
+
+  // Text
+  textWhite:     '#FFFFFF',
+  textLight:     'rgba(210, 205, 240, 0.70)',
+  textMuted:     'rgba(160, 155, 200, 0.55)',
+  textCard:      '#1A1A3E',
+  textCardMuted: '#6B6B9E',
+  textAccent:    '#5B35CC',
+
+  // Status / nav
+  navActive:     '#6B5FFF',
+  navInactive:   'rgba(130, 125, 185, 0.45)',
+  divider:       'rgba(107, 63, 212, 0.15)',
+
+  // Shadows
+  shadowPurple:  '#5B35CC',
+};
+
+export default Colors;
